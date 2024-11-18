6db5be0a6321f4136425a4549180be30989a25e4
Laassel <lassel.giovanni@gmail.com>
2024-11-18 18:09:23 +0100

    Add express app and i start the server

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const app = express();
 app.get('/',(req, res) =>{
     res.send('server del mio blog');
     
+});
+
+app.listen(3000,() =>{
+    console.log('Server in esecuzione sulla porta 3000');
 })
